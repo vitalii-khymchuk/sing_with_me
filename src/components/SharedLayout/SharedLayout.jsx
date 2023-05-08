a449c54a328b013eb1ab3fa32ae604766847933a
Vitalii Khymchuk <vitalii.workbox001@gmail.com>
2023-05-08 16:30:19 +0300

    set /details NavBar

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -2,11 +2,16 @@ import { Outlet } from "react-router-dom";
 import { Suspense } from "react";
 import NavBar from "modules/NavBar";
 import { Container } from "@chakra-ui/react";
+import { useLocation } from "react-router-dom";
 
 const SharedLayout = () => {
+  const { pathname } = useLocation();
+
+  const isDetailsPage = pathname.match(/^\/details\/\d+$/);
+
   return (
     <>
-      <NavBar />
+      {!isDetailsPage && <NavBar />}
       <Suspense fallback={null}>
         <Container>
           <Outlet />
