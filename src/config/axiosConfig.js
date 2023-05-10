b71488e31cf2fb31b04b8dd324c8eb5be152ecf6
Vitalii Khymchuk <vitalii.workbox001@gmail.com>
2023-05-10 20:12:54 +0300

    removed unused string

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const axiosClient = axios.create({ baseURL: "http://localhost:3001/api/v1" });
+const baseURL = process.env.REACT_APP_BACKEND_URL;
+const axiosClient = axios.create({ baseURL });
 
 const setBearer = (token) =>
   (axiosClient.defaults.headers.common["Authorization"] = `Bearer ${token}`);
